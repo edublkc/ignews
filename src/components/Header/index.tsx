36c4fa67edb8d0857fc1b790122f00c044b45348
edublkc <89402489+edublkc@users.noreply.github.com>
2022-03-05 13:38:55 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignInButton } from "../SignInButton"
+import styles from "./style.module.scss"
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg"/>
+                <nav>
+                    <a className={styles.active} href="">Home</a>
+                    <a href="">Posts</a>
+                </nav>
+                <SignInButton/>
+            </div>
+        </header>
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "../styles/global.scss"
+import { AppProps } from "next/app"
+import { Header } from "../components/Header"
+import { SessionProvider } from "next-auth/react"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
+
+export default MyApp
